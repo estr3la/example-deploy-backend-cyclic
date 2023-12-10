ce0aabb0d11162bb4e11c469e3953c98649e3bb4
estr3la <fane_santana14@hotmail.com>
2023-12-10 19:15:25 -0300

    new deploy

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 export class HomeController {
 	async handle(req: Request, res: Response) {
-		return res.json('Tudo ceto! A aplicação está funcionando perfeitamente.')
+		//return res.json('Tudo ceto! A aplicação está funcionando perfeitamente.')
+		return res.json('Mensagem de confirmacao de um novo deploy.')
 	}
 }
